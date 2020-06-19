b9f701f19cbe648994f48ff6ca4e879e173935a8
yerin-158 <hanblueblue@gmail.com>
2020-06-19 21:43:02 +0900

    YER-101 [test] repository를 생성하여 CRUD를 테스트한다.

--- src/main/java/com/freeboard04/domain/goodContentsHistory/GoodContentsHistoryRepository.java
+++ src/main/java/com/freeboard04/domain/goodContentsHistory/GoodContentsHistoryRepository.java
@@ -3,8 +3,10 @@ package com.freeboard04.domain.goodContentsHistory;
 import com.freeboard04.domain.board.BoardEntity;
 import com.freeboard04.domain.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GoodContentsHistoryRepository extends JpaRepository<GoodContentsHistoryEntity, Long> {
-    public Long findByUserAndBoard(UserEntity user, BoardEntity board);
-    public int countByBoard(BoardEntity boardEntity);
+    Long findByUserAndBoard(UserEntity user, BoardEntity board);
+    int countByBoard(BoardEntity boardEntity);
 }
