376f6eaf545eaf9c66557aefc151daf225e45139
yerin-158 <hanblueblue@gmail.com>
2020-06-21 16:18:32 +0900

    YER-109 [개발] 가져온 Board 정보와 좋아요 정보를 통합하여 클라이언트로 전달한다. - 테스트

--- src/test/java/com/freeboard04/domain/board/BoardServiceUnitTest.java
+++ src/test/java/com/freeboard04/domain/board/BoardServiceUnitTest.java
@@ -1,24 +1,39 @@
 package com.freeboard04.domain.board;
 
+import com.freeboard04.api.PageDto;
+import com.freeboard04.api.board.BoardDto;
 import com.freeboard04.api.user.UserForm;
+import com.freeboard04.domain.board.enums.BoardExceptionType;
 import com.freeboard04.domain.goodContentsHistory.GoodContentsHistoryEntity;
+import com.freeboard04.domain.goodContentsHistory.GoodContentsHistoryMapper;
 import com.freeboard04.domain.goodContentsHistory.GoodContentsHistoryRepository;
 import com.freeboard04.domain.goodContentsHistory.enums.GoodContentsHistoryExceptionType;
+import com.freeboard04.domain.goodContentsHistory.vo.CountGoodContentsHistoryVO;
 import com.freeboard04.domain.user.UserEntity;
 import com.freeboard04.domain.user.UserRepository;
+import com.freeboard04.domain.user.enums.UserExceptionType;
 import com.freeboard04.domain.user.enums.UserRole;
+import com.freeboard04.util.PageUtil;
 import com.freeboard04.util.exception.FreeBoardException;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -41,6 +56,9 @@ public class BoardServiceUnitTest {
     @Mock
     private GoodContentsHistoryRepository mockGoodHistoryRepo;
 
+    @Mock
+    private GoodContentsHistoryMapper mockGoodHistoryMapper;
+
     @Test
     @DisplayName("로그인한 유저와 글을 작성한 유저가 다를 경우 삭제를 진행하지 않는다.")
     public void delete1() {
@@ -51,7 +69,11 @@ public class BoardServiceUnitTest {
         given(mockUserRepo.findByAccountId(anyString())).willReturn(userLoggedIn.convertUserEntity());
         given(mockBoardRepo.findById(anyLong())).willReturn(Optional.of(boardEntity));
 
-        sut.delete(anyLong(), userLoggedIn);
+        Throwable e = assertThrows(FreeBoardException.class,
+                () -> sut.delete(anyLong(), userLoggedIn)
+        );
+
+        assertEquals(e.getMessage(), BoardExceptionType.NO_QUALIFICATION_USER.getErrorMessage());
         verify(mockBoardRepo, never()).deleteById(anyLong());
     }
 
@@ -117,5 +139,146 @@ public class BoardServiceUnitTest {
 
         assertEquals(GoodContentsHistoryExceptionType.CANNOT_FIND_HISTORY.getErrorMessage(), e.getMessage());
         verify(mockGoodHistoryRepo, never()).deleteById(anyLong());
+    }
+
+    @Test
+    @DisplayName("로그인하지 않은 경우에 데이터를 요청하면 isLike는 false로 셋팅된다.")
+    public void newGetTest1() {
+        long groupId = 1L;
+        long likePoint = 10L;
+        int pageNumber = 1;
+        int pageSize = 1;
+
+        Page<BoardEntity> spyBoardEntityPage = getSpyBoardEntityPage(groupId, pageNumber, pageSize);
+        List<CountGoodContentsHistoryVO> spyCountGoodHistory = getSpyCountGoodContentsHistoryVOS(groupId, likePoint);
+
+        given(mockBoardRepo.findAll(PageUtil.convertToZeroBasePage(PageRequest.of(pageNumber, pageSize)))).willReturn(spyBoardEntityPage);
+        given(mockGoodHistoryMapper.countByBoardIn(anyList())).willReturn(spyCountGoodHistory);
+
+        PageDto<BoardDto> result = sut.get(PageRequest.of(pageNumber, pageSize), Optional.empty());
+
+        assertThat(result.getContents().get(0).getLikePoint(), equalTo(likePoint));
+        assertThat(result.getContents().get(0).isLike(), equalTo(false));
+    }
+
+    @Test
+    @DisplayName("로그인 경우에 데이터를 요청하면 좋아요한 경우 isLike는 true로 셋팅된다.")
+    public void newGetTest2() {
+        long groupId = 1L;
+        long likePoint = 10L;
+        int pageNumber = 1;
+        int pageSize = 1;
+
+        Page<BoardEntity> spyBoardEntityPage = getSpyBoardEntityPage(groupId, pageNumber, pageSize);
+        List<CountGoodContentsHistoryVO> spyCountGoodHistory = getSpyCountGoodContentsHistoryVOS(groupId, likePoint);
+
+        given(mockBoardRepo.findAll(PageUtil.convertToZeroBasePage(PageRequest.of(pageNumber, pageSize)))).willReturn(spyBoardEntityPage);
+        given(mockGoodHistoryMapper.countByBoardIn(anyList())).willReturn(spyCountGoodHistory);
+        given(mockUserRepo.findByAccountId(anyString())).willReturn(new UserEntity());
+        given(mockGoodHistoryMapper.countByBoardInAndUser(anyList(), any())).willReturn(spyCountGoodHistory);
+
+        PageDto<BoardDto> result = sut.get(PageRequest.of(pageNumber, pageSize), Optional.of(UserForm.builder().accountId("mockTest").build()));
+
+        assertThat(result.getContents().get(0).getLikePoint(), equalTo(likePoint));
+        assertThat(result.getContents().get(0).isLike(), equalTo(true));
+    }
+
+    private Page<BoardEntity> getSpyBoardEntityPage(long groupId, int pageNumber, int pageSize) {
+        return spy(new Page<BoardEntity>() {
+            @Override
+            public int getTotalPages() {
+                return 0;
+            }
+
+            @Override
+            public long getTotalElements() {
+                return 0;
+            }
+
+            @Override
+            public <U> Page<U> map(Function<? super BoardEntity, ? extends U> converter) {
+                return null;
+            }
+
+            @Override
+            public int getNumber() {
+                return pageNumber;
+            }
+
+            @Override
+            public int getSize() {
+                return pageSize;
+            }
+
+            @Override
+            public int getNumberOfElements() {
+                return 0;
+            }
+
+            @Override
+            public List<BoardEntity> getContent() {
+                List<BoardEntity> boardEntities = spy(new ArrayList<>());
+                BoardEntity boardEntity = BoardEntity.builder().writer(new UserEntity()).build();
+                boardEntity.setId(groupId);
+                boardEntities.add(boardEntity);
+
+                return boardEntities;
+            }
+
+            @Override
+            public boolean hasContent() {
+                return false;
+            }
+
+            @Override
+            public Sort getSort() {
+                return Sort.unsorted();
+            }
+
+            @Override
+            public boolean isFirst() {
+                return false;
+            }
+
+            @Override
+            public boolean isLast() {
+                return false;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return false;
+            }
+
+            @Override
+            public Pageable nextPageable() {
+                return null;
+            }
+
+            @Override
+            public Pageable previousPageable() {
+                return null;
+            }
+
+            @Override
+            public Iterator<BoardEntity> iterator() {
+                return null;
+            }
+        });
+    }
+
+    private List<CountGoodContentsHistoryVO> getSpyCountGoodContentsHistoryVOS(long groupId, long likeCount) {
+        List<CountGoodContentsHistoryVO> countGoodContentsHistoryVOS = spy(new ArrayList<>());
+        CountGoodContentsHistoryVO vo = new CountGoodContentsHistoryVO();
+        vo.setGroupId(groupId);
+        vo.setLikeCount(likeCount);
+        countGoodContentsHistoryVOS.add(vo);
+
+        return countGoodContentsHistoryVOS;
     }
 }
