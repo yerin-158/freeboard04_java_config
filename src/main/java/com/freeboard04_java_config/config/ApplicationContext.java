8019de61527504a86a3eb6b518525928e120e27b
yerin-158 <hanblueblue@gmail.com>
2020-07-09 21:44:37 +0900

    TransactionManager, entityManagerFactory 빈 설정 추가

--- src/main/java/com/freeboard04_java_config/config/ApplicationContext.java
+++ src/main/java/com/freeboard04_java_config/config/ApplicationContext.java
@@ -1,16 +1,26 @@
 package com.freeboard04_java_config.config;
 
+import com.freeboard04_java_config.domain.board.BoardRepository;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.sql.CallableStatement;
+import java.util.Properties;
 
 @Configuration
 @ImportResource({"classpath:applicationContext.xml"})
-@EnableTransactionManagement
 public class ApplicationContext {
 
     @Bean
@@ -22,6 +32,36 @@ public class ApplicationContext {
         dataSource.setPassword("robin549866pass!");
 
         return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] {"com.freeboard04_java_config.domain"});
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty("format_sql", "true");
+        properties.setProperty("hibernate.connection.autocommit", "true");
+
+        return properties;
     }
 
 }
