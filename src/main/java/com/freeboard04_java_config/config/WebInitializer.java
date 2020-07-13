6535e691bac49a1707a9167506283581efdddaef
yerin-158 <hanblueblue@gmail.com>
2020-07-13 17:06:07 +0900

    web.xml을 대체, onStartup 메소드를 오버라이딩하여 자동 호출한다.

---
+++ src/main/java/com/freeboard04_java_config/config/WebInitializer.java
@@ -0,0 +1,39 @@
+package com.freeboard04_java_config.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.*;
+import java.util.EnumSet;
+
+public class WebInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        registerDispatcherServlet(servletContext);
+        registerCharacterEncodingFilter(servletContext);
+    }
+
+    private void registerDispatcherServlet(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(ApplicationContext.class);
+
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+        webContext.register(WebConfig.class);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(webContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/*");
+    }
+
+    private void registerCharacterEncodingFilter(ServletContext servletContext) {
+        FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        characterEncodingFilter.setInitParameter("encoding", "UTF-8");
+        characterEncodingFilter.setInitParameter("forceEncoding", "true");
+        characterEncodingFilter.addMappingForServletNames(EnumSet.allOf(DispatcherType.class), true, "/*");
+    }
+}
